d7ce397efec68d0950b065d30111d4838fbbcac7
lanin0777 <73796876+lanin0777@users.noreply.github.com>
2020-12-06 11:08:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ function myFunc(express, bodyParser, createReadStream, crypto, http) {
       res.send(hash.digest('hex'));
     })
 
-    .get('/login/', (req, res) => res.send('123'))
+    .get('/login/', (req, res) => res.send('alexlaikn'))
     .get('/code/', (req, res) => {
       let filename = import.meta.url.substring(7);
       createReadStream(filename).pipe(res);
@@ -43,7 +43,7 @@ function myFunc(express, bodyParser, createReadStream, crypto, http) {
     });
   });
   app.all('*', (req, res) => {
-    res.send('123');
+    res.send('alexlaikn');
   });
   return app;
 }
